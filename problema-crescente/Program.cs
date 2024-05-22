57e83fbd89f6c9010245e5f6a966ae83501e60c1
igorl <igorl@DESKTOP-273U8TR>
2024-05-22 17:53:33 -0300

    Adicionar arquivos de projeto.

---
+++ problema-crescente/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_crescente {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x, y;
+
+            Console.WriteLine("Digite dois numeros: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y) {
+                if (x < y) {
+                    Console.WriteLine("CRESCENTE");
+                }
+                else {
+                    Console.WriteLine("DECRESCENTE");
+                }
+
+                Console.WriteLine("Digite outros dois numeros:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
